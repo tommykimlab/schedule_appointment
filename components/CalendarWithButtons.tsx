d859e5b53cd069386194efb49ffc8025adaa25cc
cartpaul13 <tykim890813@gmail.com>
2024-01-07 20:49:57 +0700

    first commit

---
+++ components/CalendarWithButtons.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { Calendar } from "@/components/ui/calendar"
+import React, { useState } from 'react';
+import { Button } from "@/components/ui/button"
+
+const CalendarWithButtons = () => {
+  const [isCalendarClicked, setCalendarClicked] = useState(false);
+
+  const handleCalendarClick = () => {
+    setCalendarClicked(true);
+  }
+
+  const times = Array.from({length: 10}, (_, i) => 9 + i); // generates hours from 9 to 18
+
+  return (
+    <div onClick={handleCalendarClick}>
+      {!isCalendarClicked && <Calendar mode='single' className="rounded-md border"/>}
+      {isCalendarClicked && (
+        <div className="flex flex-col space-y-4">
+          {times.map(time => (
+            <Button key={time} width="100%">
+              {time}:00 - {time + 1}:00
+            </Button>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default CalendarWithButtons;
